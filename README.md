4bcda236681686a3e8df538a6dd484294c4bffb5
TheAlan404 <43997085+TheAlan404@users.noreply.github.com>
2021-02-22 18:11:07 +0300

    1.0.0

--- README.md
+++ README.md
@@ -1,2 +1,69 @@
 # nbs.js
  Parse NBS (Note Block Song / Note Block Studio)  files
+Pull Requests and issues are welcome!!
+
+# API
+
+## NBS.loadSong(filename)
+Loads a song by given filename/path.
+- filename : string
+- **Returns:** Song
+
+## NBS.parse(data)
+Parses data and returns a new Song.
+- data : buffer
+- **Returns:** Song
+
+## NBS.Song
+
+### new Song([data])
+These properties are added to the song:
+- data : object
+- data.title : string
+- data.author : string
+- data.description : string
+- data.original_author : string
+- data.imported_name : string
+- data.tempo : number (ticks per second)
+- data.length : number (length of song measured in ticks)
+- data.songHeight : number (opennbs says its count of layers, but check song.layers for that)
+- data.layers : object<Layer> (layers of the song)
+
+## NBS.Layer
+
+### new Layer()
+Creates a new layer
+
+### layer.name
+Name of the layer
+
+### layer.volume
+Volume of the layer (between 0 and 100)
+
+### layer.notes
+Object containing Note. Object's keys are the tick position of the note.
+
+### layer.setNote(tick, note)
+Sets a note on tick.
+- tick : number
+- note : Note
+
+## NBS.Note
+
+### new Note(instrument, key)
+These properties are also added to the note:
+- instrument : number (0-15, can be found [here][https://opennbs.org/nbs])
+- key : number (from 0-87, minecraft supports 33-57, [more info][https://opennbs.org/nbs])
+
+### note.pitch
+Decimal number representing the pitch for notchian clients, is set to 0 if _note.key_ is out of support range (33-57)
+
+### note.packet
+Returns a [minecraft-protocol][https://github.com/PrismarineJS/node-minecraft-protocol] packet data.
+Send to client with name of "sound_effect" to play the note.
+
+## NBS.keyToPitch
+Object used for note.pitch, perhaps you can modify it for something
+
+## NBS.instrumentIds
+Object used to make minecraft-protocol packet data. Is 1.12.2 as far as i know. Maybe fix this later
---
+++ index.js
@@ -0,0 +1,226 @@
+const ByteBuffer = require("bytebuffer");
+const fs = require("fs");
+
+
+class Song {
+	constructor(data={}){
+		this.title = data.title || ""
+		this.author = data.author || "";
+		this.description = data.description || "";
+		this.original_author = data.original_author;
+		this.imported_name = data.imported_name;
+		this.tempo = data.tempo;
+		this.length = data.length;
+		this.songHeight = data.songHeight;
+		this.layers = data.layers || {};
+	};
+};
+
+class Layer {
+	constructor(){
+		this.notes = {};
+		this.volume = 100;
+		this.name = "";
+	};
+	setNote(tick, note){
+		this.notes[tick] = note;
+	};
+};
+
+class Note {
+	constructor(instrument, key){
+		this.instrument = instrument;
+        this.key = key;
+		this.pitch = keyToPitch[this.key-33] || 0;
+	};
+	get packet(){
+		// note: packet name is "sound_effect" :3
+		return {
+			soundId: instrumentIds[this.instrument] || 76,
+			soundCategory: 0,
+			x: 0,
+			y: 0,
+			z: 0,
+			volume: 1,
+			pitch: this.pitch,
+		};
+	};
+};
+
+
+const keyToPitch = {
+	0: 0.5,
+    1: 0.53,
+    2: 0.56,
+    3: 0.6,
+    4: 0.63,
+    5: 0.67,
+    6: 0.7,
+    7: 0.76,
+    8: 0.8,
+    9: 0.84,
+    10: 0.9,
+    11: 0.94,
+    12: 1.0,
+    13: 1.06,
+    14: 1.12,
+    15: 1.18,
+    16: 1.26,
+    17: 1.34,
+    18: 1.42,
+    19: 1.5,
+    20: 1.6,
+    21: 1.68,
+    22: 1.78,
+    23: 1.88,
+    24: 2.0,
+};
+
+const instrumentIds = {
+	0: 76, // harp
+	1: 70, // wood
+	2: 71, // stone
+	3: 79, // snare/sand
+	4: 78, // glass
+	5: 75, // guitar/wool
+	6: 74, // flute/clay
+	7: 72, // bell/gold block
+	8: 73, // chime/packed ice
+	9: 80, // xylophone/bone block
+	10: 80, // xylo, but iron? TODO: fix these
+	11: null,
+	12: null,
+	13: null,
+	14: null,
+	15: null,
+};
+
+
+
+function parse(data){
+	let bb = ByteBuffer.fromHex(data.toString("hex"), true);	
+	bb.append(f);
+	bb.offset = 0;
+	
+	bb.readShort();
+	let version = bb.readByte();
+	let vanillaInstruments = bb.readByte();
+	let length = bb.readShort();
+	let songHeight = bb.readShort();
+	
+	let title = bb.readIString();
+	let author = bb.readIString();
+	let original_author = bb.readIString();
+	let description = bb.readIString();
+	
+	let tempo = bb.readShort()/100;
+	bb.readByte(); // auto save
+	bb.readByte(); // auto save dur.
+	bb.readByte(); // time sig.
+	
+	// stats
+	bb.readInt();
+	bb.readInt();
+	bb.readInt();
+	bb.readInt();
+	bb.readInt();
+	
+	let imported_name = bb.readIString();
+	bb.readByte(); // loop
+	bb.readByte(); // max loop
+	bb.readShort(); // loop start
+	
+	let layers = {};
+	
+	function setNote(layer, ticks, instrument, key) {
+        let l = layers[layer];
+        if (!l) {
+            l = new Layer();
+            layers[layer] = l;
+        }
+        l.setNote(ticks, new Note(instrument, key));
+    }
+	
+	let tick = -1;
+	while (true) {
+		jumpTicks = bb.readShort(); // jumps till next tick
+		if (jumpTicks == 0) {
+			break;
+		}
+		tick += jumpTicks;
+		let layer = -1;
+		while (true) {
+			let jumpLayers = bb.readShort(); // jumps till next layer
+			if (jumpLayers == 0) {
+				break;
+			};
+			layer += jumpLayers;
+			let instrument = bb.readByte();
+			let key = bb.readByte();
+			let velocity = bb.readByte();
+			let panning = bb.readByte();
+			let pitch = bb.readShort();
+			setNote(layer, tick, instrument, key);
+		};
+	};
+	for (let i = 0; i < songHeight; i++) {
+		let l = layers[i];
+		if (l) {
+			l.name = bb.readIString();
+			l.locked = bb.readByte();
+			l.volume = bb.readByte();
+			l.streo = bb.readByte();
+		};
+	};
+	return new Song({
+		title,
+		author,
+		description,
+		original_author,
+		imported_name,
+		tempo,
+		length,
+		songHeight,
+		layers
+	});
+};
+
+
+
+
+
+function loadSong(filename){
+	return parse(fs.readFileSync(filename));
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = {
+	loadSong,
+	parse,
+	Song,
+	Layer,
+	Note,
+	keyToPitch,
+	instrumentIds,
+};
